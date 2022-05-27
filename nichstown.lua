1221ec1de2d286179fd31cc63b24cccea2be1f9c
HeyNich <105700862+HeyNich@users.noreply.github.com>
2022-05-27 09:33:55 -0400

    Add files via upload

---
+++ nichstown.lua
@@ -0,0 +1,42 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
+local w = library:CreateWindow("Nichi's Town")
+local a = w:CreateFolder("Minigames") 
+local b = w:CreateFolder("Scavenger Hunts")
+
+a:DestroyGui()
+
+b:Button("GUCCI Day 1",function()
+	local plyr = game.Players.LocalPlayer;
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(458.898132, 24, 436.329742)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(87.8606567, 22, -86.1287079)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(254.5, 12, 121.500008)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(705.842834, 21, -148.896255)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(516.700012, 18, -268.600006)
+end)
+
+b:Button("GUCCI Day 2",function()
+	local plyr = game.Players.LocalPlayer;
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(132.600006, 18.5, -411)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(74.7513504, 18.5, 66.9350433)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(259.973419, 18.5, 509.063416)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(818.06134, 22, 272.679779)
+	wait(.4)
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(618.5, 10, -86.5)
+end)
+
+a:Button("Red/Green Light",function()
+	local plyr = game.Players.LocalPlayer;
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(79.9587784, 115, -3851.46094)
+end)
+
+a:Button("Rising Tides",function()
+	local plyr = game.Players.LocalPlayer;
+	plyr.Character.HumanoidRootPart.CFrame = CFrame.new(-552.000122, 180, -3908)
+end)
